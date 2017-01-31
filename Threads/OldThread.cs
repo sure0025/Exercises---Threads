100658cfedbf7b4ceafef79a9325a8bd258a845c
sure0025 <sure0025@edu.eal.dk>
2017-01-31 11:46:19 +0100

    Add project files.

---
+++ Threads/OldThread.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class OldThread
+    {
+
+      
+        public static void Message1()
+        {
+            bool isRunning = true;
+            int count = 0;
+            while (isRunning)
+            {
+                Console.WriteLine("C # threads is easy!");
+                count++; //add to the count
+                Thread.Sleep(1000);
+                if (count == 5)
+                {
+                    isRunning = false;
+
+                }
+            }
+        }
+           public static void Message2()
+        {
+            bool isRunning = true;
+            int count = 0;
+            while (isRunning)
+            {
+                Console.WriteLine("Also, with mulitple threads ...");
+                count++; //add to the count
+                Thread.Sleep(1000);
+                if (count == 5)
+                {
+                    isRunning = false;
+
+                }
+            }
+           
+
+        }
+
+       
+    }
+}
---
+++ Threads/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start\n");
+            
+            Thread thread1 = new Thread(new ThreadStart(OldThread.Message1));
+            thread1.Start();
+
+         //   Thread.Sleep(1000); //sleeps for 1 second
+
+            Thread thread2 = new Thread(new ThreadStart(OldThread.Message2));
+            thread2.Start();
+            
+
+            thread1.Join();
+            thread2.Join();
+            Console.WriteLine("\nEnter for Exit");
+            Console.ReadLine();
+            Console.WriteLine("EXIT");
+        }
+    }
+}
